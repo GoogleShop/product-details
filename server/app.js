fdec1565e247bc0de815f14b8e9ec84adccb7eda
redperezz <kaicruzz91@gmail.com>
2021-09-14 14:31:36 -0700

    Refactored ProductImages, removed unused code

--- server/app.js
+++ server/app.js
@@ -3,22 +3,16 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const controller = require('../database/controllers.js');
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('client/dist'));
 app.use('/shop/:id', express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/shop/product/:id', async (req, res) => {
   try {
     let productId = req.params.id;
-    console.log(productId);
     let productData = await controller.getProductData(productId);
-    console.log(productData);
     res.send(productData);
   } catch (err) {
     console.log('Error from GET: ', err);
